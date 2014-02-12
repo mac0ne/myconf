149c5bb95e14dad9825ba4daa0f3066af451c87f
mac0ne <mac0ne@users.noreply.github.com>
2014-02-12 16:39:17 +0800

    Update setup-ubuntu.sh

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -34,4 +34,5 @@ fi
 wget https://raw.github.com/mac0ne/myconf/master/.vimrc
 
 #install chrome
-curl https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb| dpkg -i
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i google-chrome-stable_current_amd64.deb
