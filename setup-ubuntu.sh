64b7891928a4612c21294e70edc12dbfcdb1a51e
mac0ne <mac0ne@users.noreply.github.com>
2014-02-14 10:47:16 +0800

    Update setup-ubuntu.sh

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -35,4 +35,5 @@ wget https://raw.github.com/mac0ne/myconf/master/.vimrc
 
 #install chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get install "-y" libxss1
 dpkg -i google-chrome-stable_current_amd64.deb
